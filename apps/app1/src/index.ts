90cb2fe77913bac5fad48d025ca93ebfc8e34e60
wuqinghua <wuqinghua@heywhale.com>
2024-05-05 02:31:48 +0800

    feat(工程化): monorepo项目规范
    eslint/prettier/commilint/husky/lintstaged

--- apps/app1/src/index.ts
+++
@@ -1,5 +0,0 @@
-import { add } from '@monorepo/utils';
-
-console.log(add(1, 2));
-console.log(add(3, 4));
-console.log(add(4, 5));
---
+++ apps/ex-app/src/index.ts
@@ -0,0 +1,3 @@
+import { add } from '@wqh-monorepo/utils';
+
+console.log(add(1, 23));
